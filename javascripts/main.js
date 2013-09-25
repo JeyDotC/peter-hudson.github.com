2862a1d57e4874a654d4b965f730169e6c080db7
Peter <peterhhudson@gmail.com>
2013-09-25 17:15:36 +0100

    SVG stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+        document.body.setAttribute('class','js');
+        var draw = SVG('canvas');
+        var image = draw.image('https://graph.facebook.com/peterhhudson/picture?width=100&height=100',100,100);
+        var circle = draw.circle(100).center(50,50);
+        image.clipWith(circle);
+
+
