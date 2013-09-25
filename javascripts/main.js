c7a461d7270d4aa58699fd2207f36533ee83e1b7
Peter <peterhhudson@gmail.com>
2013-09-25 17:21:00 +0100

    svg js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,8 @@
+    if (SVG.supported) {
         document.body.setAttribute('class','js');
         var draw = SVG('canvas');
         var image = draw.image('https://graph.facebook.com/peterhhudson/picture?width=100&height=100',100,100);
         var circle = draw.circle(100).center(50,50);
         image.clipWith(circle);
-
+    }
 
